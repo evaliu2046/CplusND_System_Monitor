50ac7ad70706b7ba07bdaa32bbd122b04b08487d
Eva Liu <evaliu2046@gmail.com>
2019-06-23 15:20:25 -0700

    Add files via upload

--- Process.h
+++ Process.h
@@ -1,3 +1,19 @@
+/**
+ * @file: Process.h
+ *
+ * @brief:
+ * 	CppND-System-Monitor: Header file for all the process representation.
+ *     
+ * @ingroup:
+ * 	CppND-System-Monitor
+ *
+ * @author:
+ * 	Eva Liu - evaliu2046@gmail.com
+ * 
+ * @date:
+ * 	2019/Jun/23
+ *
+ */
 #include <string>
 
 using namespace std;
@@ -18,11 +34,10 @@ public:
     Process(string pid){
         this->pid = pid;
         this->user = ProcessParser::getProcUser(pid);
-        //TODOs:
-        //complete for mem
-        //complete for cmd
-        //complete for upTime
-        //complete for cpu
+        this->mem = ProcessParser::getVmSize(pid);
+        this->cpu = ProcessParser::getCpuPercent(pid);
+        this->upTime = ProcessParser::getProcUpTime(pid);
+        this->cmd = getCmd(pid);
     }
     void setPid(int pid);
     string getPid()const;
@@ -39,6 +54,15 @@ void Process::setPid(int pid){
 string Process::getPid()const {
     return this->pid;
 }
+
+/**
+ * @function:
+ *  string Process::getProcess();
+ *  This function gets the process information.
+ *
+ * @param: NULL
+ * @return: process information.
+ */
 string Process::getProcess(){
     if(!ProcessParser::isPidExisting(this->pid))
         return "";
@@ -46,5 +70,9 @@ string Process::getProcess(){
     this->upTime = ProcessParser::getProcUpTime(this->pid);
     this->cpu = ProcessParser::getCpuPercent(this->pid);
 
-    return (this->pid + "   " + //TODO: finish the string! this->user + "   "+ mem...cpu...upTime...;
+    return (this->pid + "   " + this->user 
+            + "   " + this->mem.substr(0,5) 
+            + "   " + this->cpu.substr(0,5)
+            + "   " + this->upTime.substr(0,5)
+            + "   " + this->cmd.substr(0,30));
 }
